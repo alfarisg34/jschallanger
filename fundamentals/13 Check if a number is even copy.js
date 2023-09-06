4fb1400b815c618022af40466a52ea7b6e14ccd3
Alfarisg 34 <alfarisg34@gmail.com>
2023-09-06 19:27:24 +0700

    13

---
+++ fundamentals/13 Check if a number is even copy.js
@@ -0,0 +1,14 @@
+//my solution
+function myFunction ( a ) {
+	return a%2==0 ? true : false;
+}
+
+//The author's solution
+// function myFunction(a) {
+//     return a % 2 === 0
+//   }
+
+console.log(myFunction(10))
+console.log(myFunction(-4))
+console.log(myFunction(5))
+console.log(myFunction(-111))
